98becffb786de83bb45a5b1565de5c62395be6cb
minister125 <minister1257@gmail.com>
2025-02-27 00:53:44 -0800

    add

--- packtracSite/settings.py
+++ packtracSite/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*u52(zk6@#n2i@x$)h*o9)vwp!2s==a)q%)q7bfxd&e55h0xw1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://qwog808okco0w4o0cwoo4gk8.famcitysch.org']
+ALLOWED_HOSTS = ['famcitysch.org', 'qwog808okco0w4o0cwoo4gk8.famcitysch.org']
 
 
 # Application definition
