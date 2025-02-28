78e827e462da68e62fe508e5837006a4c0612e0f
minister125 <minister1257@gmail.com>
2025-02-28 08:34:03 -0800

    add

--- Package_tracking/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class TrackingForm(forms.Form):
-    tracking_id = forms.UUIDField(
-         
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your tracking code. e.g. 987733733-GT'}),
-    )
